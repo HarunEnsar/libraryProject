55613a3fd5dafc46587e7fb3625cfc451111cca2
HarunEnsar <99835656+HarunEnsar@users.noreply.github.com>
2023-07-11 17:15:01 +0300

    20230711123455_CreateKisilerTable
    added migration for kisi table

---
+++ library/Migrations/20230711123455_CreateKisilerTable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace library.Migrations
+{
+    public partial class CreateKisilerTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kisiler",
+                columns: table => new
+                {
+                    aliciId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    alici_no = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kisiler", x => x.aliciId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Kisiler");
+        }
+    }
+}
