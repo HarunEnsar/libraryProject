9c25411e1ae489740f121092986f6a5c278d4624
HarunEnsar <99835656+HarunEnsar@users.noreply.github.com>
2023-07-13 00:15:56 +0300

    odunc.cs
    Created odunc.cs (used Ef core codefirst model) for odunc table

---
+++ library/Model/odunc.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace library.Model
+{
+    public class odunc
+    {
+        [Key]
+        public int alıcıId { get; set; }
+
+
+        [Display(Name = "Alıcı No")]
+        public string? alici_no { get; set; }
+        
+        [Display(Name = "Başlık")]
+        public string? Baslik { get; set; }
+
+        [Display(Name = "Kitap No")]
+        public int kitap_no { get; set; }
+
+        [Display(Name = "Ödünç Alınma Zamanı")]
+        public DateTime OduncAlinmaZamani { get; set; } = DateTime.Now;
+    }
+}
